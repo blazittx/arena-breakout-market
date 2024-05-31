0ac2b3c4a3ee7b6bd10a4a2c4436f711b028be30
blazittx <info@doruksasmaz.com>
2024-06-01 00:22:16 +0300

    Quick Fix on App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ItemList from './components/ItemList';
 import './App.css';
